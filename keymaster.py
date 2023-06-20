3adaf24ab91ce8478fa0a21ffdfe5370cc602b96
tymothy001 <65349103+tymothy001@users.noreply.github.com>
2023-06-20 16:45:57 +0200

    Create keymaster.py

---
+++ keymaster.py
@@ -0,0 +1,160 @@
+#pip3 install progress progressbar2 alive-progress tqdm
+from tinyec.ec import SubGroup, Curve
+from progress.bar import Bar
+print("                                                                                           ")
+print("                                                                                           ")
+print("\033[0;32m██╗  ██╗███████╗██╗   ██╗    ██╗  ██╗███╗   ███╗ █████╗ ███████╗████████╗███████╗██████╗ \033[00m")
+print("\033[0;32m██║ ██╔╝██╔════╝╚██╗ ██╔╝    ╚██╗██╔╝████╗ ████║██╔══██╗██╔════╝╚══██╔══╝██╔════╝██╔══██╗\033[00m")
+print("\033[0;32m█████╔╝ █████╗   ╚████╔╝      ╚███╔╝ ██╔████╔██║███████║███████╗   ██║   █████╗  ██████╔╝\033[00m")
+print("\033[0;32m██╔═██╗ ██╔══╝    ╚██╔╝       ██╔██╗ ██║╚██╔╝██║██╔══██║╚════██║   ██║   ██╔══╝  ██╔══██╗\033[00m")
+print("\033[0;32m██║  ██╗███████╗   ██║       ██╔╝ ██╗██║ ╚═╝ ██║██║  ██║███████║   ██║   ███████╗██║  ██║\033[00m")
+print("\033[0;32m╚═╝  ╚═╝╚══════╝   ╚═╝       ╚═╝  ╚═╝╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚══════╝╚═╝  ╚═╝\033[00m")
+
+print("\033[0;32m                      ██████████                                                            \033[00m")
+print("\033[0;32m                  ████░░░░░░░░░░██████                    ░░                                \033[00m")
+print("\033[0;32m                ██░░░░░░░░░░░░░░░░░░░░██                                                    \033[00m")
+print("\033[0;32m              ██░░░░░░░░░░░░░░░░░░░░░░░░██    ████                                          \033[00m")
+print("\033[0;32m            ▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░██  ██░░██                                          \033[00m")
+print("\033[0;32m            ██░░░░░░░░░░░░░░░░░░░░░░░░░░░░██░░░░████████████████████████████████            \033[00m")
+print("\033[0;32m          ██░░░░░░██████░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██          \033[00m")
+print("\033[0;32m          ██░░░░██      ██░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██        \033[00m")
+print("\033[0;32m          ██░░░░██      ▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██          \033[00m")
+print("\033[0;32m          ██░░░░██      ██░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓░░░░░░░░▓▓░░░░░░░░██            \033[00m")
+print("\033[0;32m          ██░░░░░░██████░░░░░░░░░░░░░░░░░░░░░░░░██████░░██  ██░░░░██  ████████              \033[00m")
+print("\033[0;32m            ██░░░░░░░░░░░░░░░░░░░░░░░░░░░░██░░░░██    ██      ████                          \033[00m")
+print("\033[0;32m            ██░░░░░░░░░░░░░░░░░░░░░░░░░░██  ██░░██                                          \033[00m")
+print("\033[0;32m             ██░░░░░░░░░░░░░░░░░░░░░░░░██    ████                                          \033[00m")
+print("\033[0;32m                ██░░░░░░░░░░░░░░░░░░░░██                                                    \033[00m")
+print("\033[0;32m                  ████░░░░░░░░░░██████                                                      \033[00m")
+print("\033[0;32m                  ░░░░▓▓▓▓▓▓▓▓▓▓░░\033[00m")
+
+
+
+
+
+
+print("          keymaster V.1 test                                                                                 ")
+print("                                                                                           ")
+print("Program generuje adresy w postaci liczb za pomocą koordynatów x,y z klucza publicznego                                                     ")
+print("Użyj komendy do uzyskania punktów x oraz y                                                                                           ")
+print("W pliku m2.txt podane są liczby mnożenia                                                                                         ")
+print("                                                                                           ")
+print("python3 ku.py 02e87eedf7c1c91f3acc384c5673f78076650558e0b5596dca1fc21f24050c9bcf")
+print("                                                                                           ")
+print("python3 ku.py 04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f")
+
+
+
+print("                                                                                           ")
+print("      Koordynaty pierwszego klucza btc                                                                                    ")
+print("                                                                                           ")
+print("      x:  79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798                                                                  ")
+print("      y:  483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8                                                                                   ")
+print("                                                                                           ")
+print("      1AoeP37TmHdFh8uN72fu9AqgtLrUwcv2wJ                                                                                     ")
+print("                                                                                           ")
+print("      x:  5cd1854cae45391ca4ec428cc7e6c7d9984424b954209a8eea197b9e364c05f6                                                                                     ")
+print("      y:  f5faaa72c795ca7bd49cbc9724639002935de3acc61172e44730872e3ecb49db                                                                                     ")
+print("                                                                                           ")
+print("      112imq6hQuz4JgRH4C8mC437UnZbfN9QKh                                                                                     ")
+print("                                                                                           ")
+print("      x: 5d8c105c36695b90261f285c97eec138fc4e2a313b81ed6b34e4b012c0323c2e                                                                                    ")
+print("      y: 837da163348c1d049ef33cfc18ce7de2adc4118d05b4595a6a62ebc31fc9ded2                                                                                    ")
+print("                                                                                           ")
+print("      Bitcoin address uncompressed : 1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa                                                                                     ")
+print("      Bitcoin address              : 1FYPDCP1uVnPgEE3gaDMbAApdv9XYX7Si5                                                                                     ")
+print("      pub   04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f                                                                                   ")
+print("      x: 678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb6                                                                                    ")
+print("      y: 49f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f                                                                                    ")
+print("                                                                                           ")
+print("                                                                                           ")
+print("                                                                                           ")
+print("                                                                                           ")
+print("                                                                                           ")
+print("                                                                                           ")
+# Pobranie wartości publicznego klucza
+X = int((input("Please Enter Your Public Key X Coordinate In Hex Format: ")),16)
+Y = int((input("Please Enter Your Public Key Y Coordinate In Hex Format: ")),16)
+
+# Pobranie wartości do generowania listy
+AA = int(input("Please Enter The Size Of The Refrence List You Would Like To Create:"))
+multipliers = []
+
+# Wczytanie wartości mnożenia z pliku tekstowego
+with open("m2.txt", "r") as f:
+    for line in f:
+        multipliers.append(int(line.strip()))
+
+# Ustawienie parametrów krzywej eliptycznej
+name = 'secp256k1'
+p = 0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f
+n = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
+a = 0x0000000000000000000000000000000000000000000000000000000000000000
+b = 0x0000000000000000000000000000000000000000000000000000000000000007
+g = (X,Y)
+h = 1
+curve = Curve(a, b, SubGroup(p, g, n, h), name)
+
+# Generowanie listy kluczy
+keys = []
+with Bar('Generating keys                ', fill='░', suffix='%(percent).1f%% - %(eta)ds') as bar:
+    for i in range(1, AA+1):
+        privKey = i
+        pubKey = curve.g * privKey
+
+        for multiplier in multipliers:
+            s1 = pubKey * int(multiplier)
+            keys.append(s1.x)
+
+        # Aktualizacja paska postępu
+        progress = (i / AA) * 100
+        bar.goto(progress)
+
+# Zapisanie kluczy do pliku tekstowego
+with open('keys.txt', 'w') as e:
+    for key in keys:
+        e.write(str(key) + '\n')
+
+print("List Created...Please See File Named keys.txt")
+
+# Konwersja kluczy na adresy Bitcoin i zapisanie ich do pliku
+from bitcoinlib.keys import Key
+
+with open('dec.txt', 'r') as file:
+    lines = file.readlines()
+
+keys = []
+total_lines = len(lines)
+current_line = 0
+
+with Bar('Converting to Bitcoin addresses', fill='█', suffix='%(percent).1f%% - %(eta)ds') as bar:
+    for line in lines:
+        # Konwertowanie liczby z pliku na klucz adresu Bitcoin
+        number = int(line.strip())
+        key = Key(number)
+        keys.append(key)
+
+        # Aktualizowanie postępu ładowania danych
+        current_line += 1
+        progress = (current_line / total_lines) * 100
+        bar.goto(progress)
+
+# Zapisanie adresów Bitcoin do pliku
+with open('addr.txt', 'w') as file:
+    for key in keys:
+        # Zapisywanie klucza adresu Bitcoin do pliku
+        file.write(key.address() + '\n')
+
+print('Conversion finished. Bitcoin addresses saved in addr.txt file.')
+print("                                                                                           ")
+print(" -----------------------------------------------------------------------------------       ")
+print("                                                                                           ")
+print("                                                                                           ")
+filename = 'addr.txt'
+search_terms = input("Wprowadź słowa kluczowe oddzielone spacją: ").split()
+print("                                                                                           ")
+with open(filename, 'r') as f:
+   for line in f:
+       for term in search_terms:
+           if term in line:
+                print(line.strip())
