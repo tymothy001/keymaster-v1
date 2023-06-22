5fd4aea5722c9aeb8d61f4015b3c0349026947c9
tymothy001 <65349103+tymothy001@users.noreply.github.com>
2023-06-22 18:43:28 +0200

    Create eccgen.py

---
+++ eccgen.py
@@ -0,0 +1,31 @@
+from libnum import ecc
+import sys
+
+f = open("output3.txt", "a")
+
+def printPoints(name,a,b,p,G,scale):
+  print (name)
+  c = ecc.Curve(a,b,p,G)
+  for n in range(1,2000000000000000):
+    res = c.power(G,n)
+    print (n,"",res, file=f)
+    x,y = res
+    print (hex(x),hex(y))
+  res = c.power(G,scale)
+  print (scale,"",res, file=f)
+  x,y = res
+  print (hex(x),hex(y))
+
+scale=2
+if (len(sys.argv)>1):
+	scale=int(sys.argv[1])
+
+a=0
+b=7
+G=(55066263022277343669578718895168534326250603453777594175500187360389116729240, 32670510020758816978083085130507043184471273380659243275938904335757337482424)
+#G=(46833799212576611471711417854818141128240043280360231002189938627535641370294, 33454781559405909841731692443380420218121109572881027288991311028992835919199)
+#G=(1, 1)
+p=115792089237316195423570985008687907853269984665640564039457584007908834671663
+
+printPoints("\nsecp256k1",a,b,p,G,scale)
+f.close()
